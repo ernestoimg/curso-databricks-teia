a0f7eb93553b2ff2b9dff597b30a1d7e527d1be8
ernestoimg <ernestoimg>
2024-05-03 16:31:52 +0000

    test
    tes

---
+++ Introduccion/Functions.scala
@@ -0,0 +1,9 @@
+// Databricks notebook source
+import org.apache.spark.sql.types.StructType
+import org.apache.spark.sql.types.StructField
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.functions._
+
+// COMMAND ----------
+
+dbutils.notebook.exit("success")
